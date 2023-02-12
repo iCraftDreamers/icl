72bc5e74a65feaee7f4e6f34946791a839d4ec85
sheip9 <sheip9@outlook.com>
2023-02-13 00:32:01 +0800

    test<AccountPage>: 账号页面的基本布局和账号展示(not finish yet)

--- lib/routes/account.dart
+++ lib/routes/account.dart
@@ -1,11 +1,224 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:get/get.dart';
+
+import '../controllers/accounts.dart';
+import '../utils/accounts.dart';
 
 class AccountPage extends StatelessWidget {
   const AccountPage({super.key});
 
+  // get groupValue => null;
+
+  // get onChanged => null;
+
+  // get value => null;
+  Widget addAccountsDialog() {
+    final c = Get.put(AccountsController());
+    const data = {
+      0: '离线模式',
+      1: '正版登录',
+      2: '外置登录',
+    };
+
+    Widget typefield(
+        String title, TextEditingController controller, bool obscureText) {
+      //定义输入框, title 为输入框前的标题, describe为输入框内的描述
+      return Row(
+        children: [
+          SizedBox(
+            width: 60,
+            child: Align(alignment: Alignment.centerRight, child: Text(title)),
+          ),
+          const SizedBox(width: 15),
+          Expanded(
+            child: TextFormField(
+              controller: controller,
+              obscureText: obscureText,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return '此处不得留空！';
+                }
+                return null;
+              },
+            ),
+          ),
+        ],
+      );
+    }
+
+    List<Widget> children(int loginMode) {
+      switch (loginMode) {
+        case 1:
+          return [];
+        case 2:
+          return [
+            typefield("用户名:", c.loginUsername, false),
+            const SizedBox(height: 15),
+            typefield("密码:", c.loginPassword, true)
+          ];
+        default:
+          return [typefield("用户名:", c.loginUsername, false)];
+      }
+    }
+
+    return AlertDialog(
+      title: const Text("添加用户"),
+      content: SizedBox(
+        width: 400,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Row(
+              children: [
+                const Text("登录方式:"),
+                const SizedBox(width: 15),
+                SizedBox(
+                  width: 100,
+                  child: Obx(
+                    () => DropdownButton(
+                      isExpanded: true,
+                      value: c.loginMode.value,
+                      items: data.keys
+                          .map(
+                            (value) => DropdownMenuItem(
+                              value: value,
+                              child: Container(
+                                alignment: Alignment.center,
+                                child: Text(
+                                  data[value]!,
+                                  style: Get.textTheme.titleSmall,
+                                ),
+                              ),
+                            ),
+                          )
+                          .toList(),
+                      onChanged: (value) => c.loginMode(value),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 15),
+            Obx(
+              () => Form(
+                key: c.formKey,
+                child: Column(
+                  children: children(c.loginMode.value),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      actions: [
+        FilledButton(
+          onPressed: () {
+            if (c.formKey.currentState!.validate()) {
+              c.addAccount();
+              Get.back();
+              c.loginUsername.clear();
+              c.loginPassword.clear();
+            }
+          },
+          child: const Text("确定", style: TextStyle(fontSize: 16)),
+        ),
+        TextButton(
+          onPressed: () => Get.back(),
+          child: const Text("取消", style: TextStyle(fontSize: 16)),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    var value;
+    var groupValue;
+    var onChanged;
+    List<Widget> searchAccounts() {
+      List<Widget> children = [];
+      AccountsManaging.gameAccounts.forEach((e) {
+        children.add(
+          Padding(
+            padding: EdgeInsets.all(5),
+            child: Container(
+              height: 75,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(7.5),
+                  border: Border.all(
+                    color: Colors.black12,
+                  )),
+              child: Row(
+                children: [
+                  // const SizedBox(width: 5),
+                  Radio(
+                      value: value,
+                      groupValue: groupValue,
+                      onChanged: onChanged),
+                  Image.asset(
+                    'steve.png',
+                    height: 30,
+                    width: 30,
+                  ),
+                  const SizedBox(width: 10),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(height: 15),
+                      Text(
+                        e['username'].toString(),
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      });
+      return children;
+    }
+
+    return Column(
+      children: [
+        Expanded(
+          child: ListView(
+            padding: const EdgeInsets.all(15),
+            children: [
+              Row(
+                children: [
+                  const Text("账号列表", style: TextStyle(fontSize: 32)),
+                  const Spacer(),
+                  ElevatedButton(
+                    onPressed: () => showDialog(
+                      context: Get.context!,
+                      builder: (context) => addAccountsDialog(),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      child: Row(
+                        children: const [
+                          Icon(Icons.add),
+                          Text("添加用户"),
+                          SizedBox(width: 7),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 10),
+              Column(
+                children: searchAccounts(),
+              )
+            ],
+          ),
+        ),
+        // const Divider(height: 1),
+      ],
+    );
   }
 }
