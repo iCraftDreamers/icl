bf29a2567b2c8ee47e9944efa435e06a197a69ad
sheip9 <sheip9@outlook.com>
2023-02-16 22:46:07 +0800

    fix<Skin>: 修改对皮肤文件高的判断

--- lib/utils/skin.dart
+++ lib/utils/skin.dart
@@ -6,7 +6,7 @@ class Skin {
   static Uint8List toAvatar(file) {
     final source = decodePng(File(file).readAsBytesSync());
     int wratio = source!.width ~/ 64;
-    int lratio = (source.height == source.width) ? wratio : wratio ~/ 2;
+    int lratio = (source.height == source.width) ? wratio : source.height ~/ 32;
     final face = copyResize(
         copyCrop(source, x: 8 * wratio, y: 8 * lratio, width: 8, height: 8),
         width: 64,
