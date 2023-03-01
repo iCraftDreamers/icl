76c457445e5bb68fbfc2c4c5dff4e434abfd50a2
sheip9 <sheip9@outlook.com>
2023-03-01 12:20:00 +0800

    Remove dead codes and add notes

--- lib/utils/get_game.dart
+++ lib/utils/get_game.dart
@@ -4,23 +4,26 @@ import 'package:archive/archive.dart';
 
 class Game {
   late final jsonData;
-  late final String path;
-  late final String jar;
-  late final String id;
-  late final String version;
-  late final String? Forge;
-  late final String? OptiFine;
+  late final String path; //版本文件夹路径
+  late final String jar; //jar文件路径
+  late final String id; //游戏名
+  late final String version; //游戏版本
+  late final String? Forge; //Forge版本
+  late final String? OptiFine; //OptiFine版本
 
   Game(this.jsonData, this.path, this.jar) {
+    //初始化对象
     id = decodeID();
     version = decodeVersion();
   }
 
   String decodeID() {
+    //解析游戏id
     return jsonData['id'];
   }
 
   String decodeVersion() {
+    //解析游戏版本
     String fromJar() {
       final by = jsonDecode(utf8.decode(ZipDecoder()
           .decodeBytes(File(jar).readAsBytesSync())
@@ -35,8 +38,10 @@ class Game {
 }
 
 class GameManaging {
-  static List<Game> installedGames = [];
+  static List<Game> installedGames = []; //以对象存储已安装的游戏
+  // e.g. GameManaging.installedGames[下标].version 获取版本
   static List<Directory> gameDirs = [
+    //初始化游戏安装目录
     Directory('.minecraft'),
     Directory('%appdata%/Roadming/.minecraft')
   ];
@@ -46,6 +51,7 @@ class GameManaging {
   }
 
   static File convertToFile(FileSystemEntity dir, String type) {
+    //当系统为Windows时转换斜杠，防止无法获取当前文件夹名
     return File(
         '${dir.path}/${dir.path.split(Platform.isWindows ? '\\' : '/').last}.${type}');
   }
@@ -54,6 +60,7 @@ class GameManaging {
     installedGames.clear();
     List<FileSystemEntity> versionDirs = [];
     for (var dir in gameDirs) {
+      //遍历游戏安装目录下的已安装游戏文件夹
       var versionDir = Directory('${dir.path}/${'versions'}');
       if (await versionDir.exists()) {
         versionDirs.addAll(
@@ -65,10 +72,7 @@ class GameManaging {
       }
     }
     for (var dir in versionDirs) {
-      // final jarFile = convertToFile(dir, "jar");
-      // final jsonFile = convertToFile(dir, "json");
-      // final jsonContent = jsonDecode(await jsonFile.readAsString());
-      // Map<String, dynamic> jsonData = jsonContent;
+      //解析已安装游戏的目录内的json文件，并以对象存储
       installedGames.add(Game(
           jsonDecode(await convertToFile(dir, "json").readAsString()),
           dir.path,
