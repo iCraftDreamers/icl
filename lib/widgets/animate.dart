b7ebf7e747f8928b0d36d44e35bb00fbec9578ab
ClaretWheel1481 <245922575@qq.com>
2023-06-12 17:44:07 +0800

    最后优化Snackbar

--- lib/widgets/animate.dart
+++ lib/widgets/animate.dart
@@ -6,11 +6,11 @@ void showcustomsnackbar(String messages) {
     message: messages,
     snackStyle: SnackStyle.FLOATING,
     borderRadius: 12.5,
-    animationDuration: const Duration(milliseconds: 450),
+    animationDuration: const Duration(milliseconds: 200),
     duration: const Duration(seconds: 1),
-    margin: const EdgeInsets.only(bottom: 15),
-    maxWidth: 400,
+    margin: const EdgeInsets.only(bottom: 10),
+    maxWidth: Get.width *0.5,
     backgroundColor: Theme.of(Get.context!).colorScheme.primary,
-    icon: Icon(Icons.check),
+    icon: const Icon(Icons.check),
   ));
 }
