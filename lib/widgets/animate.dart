bc4774f2282c8d4f5e5af6f8df928516f3d433ef
ClaretWheel1481 <245922575@qq.com>
2023-06-12 15:46:35 +0800

    改进Snackbar样式

--- lib/widgets/animate.dart
+++ lib/widgets/animate.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart' hide Dialog;
 import 'package:get/get.dart';
 
-void showcustomsnackbar(String message) {
+void showcustomsnackbar(String messages) {
   Get.showSnackbar(GetSnackBar(
-    message: message,
-    snackPosition: SnackPosition.BOTTOM,
+    message: messages,
+    snackStyle: SnackStyle.FLOATING,
     borderRadius: 12.5,
     animationDuration: const Duration(milliseconds: 450),
     duration: const Duration(seconds: 1),
     margin: const EdgeInsets.only(bottom: 15),
-    maxWidth: 450,
+    maxWidth: 400,
+    backgroundColor: Theme.of(Get.context!).colorScheme.primary,
+    icon: Icon(Icons.check),
   ));
 }
